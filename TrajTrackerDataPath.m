9ef6fb1bbd49f7263dd56fa2a5aed31e7036ff21
droralef <dror.dotan@gmail.com>
2017-05-22 11:59:31 +0200

    c

--- TrajTrackerDataPath.m
+++ TrajTrackerDataPath.m
@@ -4,8 +4,6 @@ function p = TrajTrackerDataPath
 % The paths of files/datasets in this toolbox are usually specified with
 % respect to the base path, which is defined by this function.
 % 
-% If you have just installed the toolbox, this file is called
-% "TrajTrackerDataPath.m.txt", so you should remove the ".txt" extension.
 
     p = '/path/to/your/base/data/directory';
 
