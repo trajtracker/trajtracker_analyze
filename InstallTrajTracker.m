d1829c1cf01162fb98d18e39b8ffa33ecc0eac8f
droralef <dror.dotan@gmail.com>
2018-01-24 14:15:25 +0200

    Fixed the ttrk installer - it didn't work

--- InstallTrajTracker.m
+++ InstallTrajTracker.m
@@ -6,23 +6,33 @@ function InstallTrajTracker()
     fprintf('   TrajTracker toolbox installation\n');
     fprintf('   ================================\n');
     
+    if is_already_installed()
+        return;
+    end
+    
     fprintf('Please enter the directory under which you plan to store all your TrajTracker results.\n');
     fprintf('Each data directory in TrajTracker is defined by its location relatively to this base directory.\n');
     
     root_path = '';
-    while ~exist(root_path, 'dir')
+    ok = false;
+    while ~ok
         root_path = input('Root data path (default = /): ', 's');
         if isempty(root_path)
             root_path = '/';
         end
+        if exist(root_path, 'dir')
+            ok = true;
+        else
+            fprintf('This directory does not exist\n');
+        end
     end
     
-    toolbox_filename = 'TrajTracker.mltbx';
-    toolbox_path = '' + toolbox_filename;
-    local_filename = tempdir + '/' + toolbox_filename;
+    toolbox_filename = 'TrajTrackerAnalyze.mltbx';
+    toolbox_url = ['https://github.com/trajtracker/trajtracker_analyze/raw/master/' toolbox_filename];
+    local_filename = [tempdir '/' toolbox_filename];
     
     fprintf('Downloading latest version of TrajTracker...\n');
-    websave(local_filename, toolbox_path);
+    websave(local_filename, toolbox_url);
     fprintf('Done.\n\n');
     
     fprintf('Installing toolbox...\n');
@@ -30,18 +40,50 @@ function InstallTrajTracker()
     
     fprintf('%s version %s was installed\n\n', toolbox.Name, toolbox.Version);
     
-    ttrk_dir = toolboxdir(toolbox.Name);
+    ttrk_dir = get_toolbox_dir();
+    fprintf('The TrajTracker toolbox was installed in %s\n', ttrk_dir);
     
-    fprintf('\nThe TrajTracker toolbox was installed in %s\n', ttrk_dir);
-    
-    addpath(strcat(ttrk_dir, '/util'));
-    savepath();
-    
-    create_root_path_func(ttrk_dir, root_path)
+    create_root_path_func(ttrk_dir, root_path);
     
     fprintf('\n\nSuccessfully installed TrajTracker\n');
     fprintf('\nFor more information, visit us at http://www.trajtracker.com\n');
     
+    %------------------------------------------------------------------
+    function installed = is_already_installed()
+        
+        installed = false;
+        for tbx = matlab.addons.toolbox.installedToolboxes()
+            if strcmp(tbx.Name, 'TrajTracker Analyze')
+                fprintf('TrajTracker Analyze version %s is already installed.\n', tbx.Version);
+                
+                answer = '';
+                while ~ismember(answer, {'U', 'C'})
+                    answer = upper(input('Do you want to (U)install it or (C)ancel? ', 's'));
+                end
+                
+                if answer == 'U'
+                    % Uninstall
+                    matlab.addons.toolbox.uninstallToolbox(tbx);
+                    fprintf('The previous version was uninstalled\n\n\n');
+                    break;
+                else
+                    % Cancel
+                    installed = true;
+                end
+                
+            end
+        end
+        
+    end
+
+    %------------------------------------------------------------------
+    function d = get_toolbox_dir()
+        d = fileparts(which('NLExperimentData'));
+        if isempty(d)
+            error('The installation seems to have succeeded but I could not locate the installation directory');
+        end
+    end
+
     %------------------------------------------------------------------
     function create_root_path_func(directory, root_path)
         
